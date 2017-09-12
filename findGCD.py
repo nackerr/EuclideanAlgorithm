b699117057ac304e0a4c86548614714ec4ff78a3
Ryan Nacker <nackerr@rider.edu>
2017-09-11 23:14:08 -0400

    Updated the whole code
    Updated everything. Did the calculations under the roof of a function. It checks the input to be an integer, and if so also checks if the number entered is positive.

--- findGCD.py
+++ findGCD.py
@@ -1,26 +1,38 @@
-
 #! Author: Ryan Nacker
-#! Title: Euclidean Algorithm to Calculate GCD
+#! Title: Finding the GCD using the Euclidean Algorithm
 #! Email: nackerr@rider.edu
+#! Website: http://nackerr.me
 
+def calcGCD(numX, numY):
+    numQ = numX // numY
+    numR = numX % numY
 
-numX = int(input("Pleae enter the first number: "))
-numY = int(input("Please enter the second number: "))
+    numList = list([numX, numY, numQ, numR])
 
-numQ = numX // numY
-numR = numX % numY
-
-numList = list([numX, numY, numQ, numR])
-
-print("A: " + str(numList[0]) + "     B: " + str(numList[1]) + "     Q: " + str(numList[2]) + "     R:     " + str(numList[3]))
-
-while(numList[3] > 0):
-    numList[0] = numList[1]
-    numList[1] = numList[3]
-    numList[2] = numList[0] // numList[1]
-    numList[3] = numList[0] % numList[1]
     print("A: " + str(numList[0]) + "     B: " + str(numList[1]) + "     Q: " + str(numList[2]) + "     R:     " + str(numList[3]))
 
+    while(numList[3] > 0):
+        numList[0] = numList[1]
+        numList[1] = numList[3]
+        numList[2] = numList[0] // numList[1]
+        numList[3] = numList[0] % numList[1]
+        print("A: " + str(numList[0]) + "     B: " + str(numList[1]) + "     Q: " + str(numList[2]) + "     R:     " + str(numList[3]))
+    return (str(numList[1]) + " is the GCD of the two numers.")
 
+
+while True:
+    numX = input("Please enter the first number: ")
+    numY = input("Please enter the second number: ")
+    
+    try:
+        val = int(numX)
+        val2 = int(numY)
+        if val < 0 or val2 < 0: 
+            print("Please input a positive number.")
+            continue
+        break
+    except ValueError:
+        print("The input you entered is not a number.")
+print(calcGCD(int(numX), int(numY)))
 
 
